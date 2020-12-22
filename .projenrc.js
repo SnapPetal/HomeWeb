91bbc4fa59e17d35d8f22ec03edcc753e19ca5a1
Thon Becker <thon.becker@gmail.com>
2020-12-22 09:18:55 +0000

    Reset with mult-project configuration

---
+++ .projenrc.js
@@ -0,0 +1,28 @@
+const {
+  Project,
+  web,
+  AwsCdkTypeScriptApp
+} = require('projen');
+
+const root = new Project({
+  name: 'HomeWeb',
+});
+
+new web.ReactTypeScriptProject({
+  parent: root,
+  outdir: 'client',
+  buildWorkflow: false,
+  releaseWorkflow: false,
+  rebuildBot: false,
+});
+
+new AwsCdkTypeScriptApp({
+  parent: root,
+  outdir: 'pipeline',
+  cdkVersion: '1.79.0',
+  buildWorkflow: false,
+  releaseWorkflow: false,
+  rebuildBot: false,
+});
+
+root.synth();
