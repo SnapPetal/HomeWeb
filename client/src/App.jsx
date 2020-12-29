04106cf0c7ca409545005a5fa8fcd5a2fa255131
Thon Becker <thon.becker@gmail.com>
2020-12-29 01:39:00 +0000

    Add about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
+import { Link, Route } from "react-router-dom";
 import { ThemeProvider, useMediaQuery } from "@material-ui/core";
 import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
@@ -21,6 +22,8 @@ import DescriptionIcon from '@material-ui/icons/Description';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import AboutPage from './AboutPage';
+import ExperiencePage from './ExperiencePage';
 
 const drawerWidth = 240;
 
@@ -153,13 +156,13 @@ export default function PersistentDrawerLeft() {
           </div>
           <Divider />
           <List component="nav">
-            <ListItem button key='About'>
+            <ListItem component={Link} to="/" button key='About'>
               <ListItemIcon>
                 <PersonIcon />
               </ListItemIcon>
               <ListItemText primary='About' />
             </ListItem>
-            <ListItem button key='Experience'>
+            <ListItem component={Link} to="/experience" button key='Experience'>
               <ListItemIcon>
                 <ShowChartIcon />
               </ListItemIcon>
@@ -194,30 +197,9 @@ export default function PersistentDrawerLeft() {
           })}
         >
           <div className={classes.drawerHeader} />
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-            facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-            tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-            consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-            vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-            hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-            tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-            nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-          </Typography>
         </main>
+      <Route exact path="/"><AboutPage /></Route>
+      <Route exact path="/experience"><ExperiencePage /></Route>
       </div>
     </ThemeProvider>
   );
