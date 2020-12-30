cc93fd4fcc6f99963d1bdc2f6aae3642d324f898
Thon Becker <thon.becker@gmail.com>
2020-12-30 15:54:11 +0000

    Fix job experiene

--- client/src/ExperiencePage.jsx
+++ client/src/ExperiencePage.jsx
@@ -1,53 +1,34 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
+import CardMedia from '@material-ui/core/CardMedia';
+import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
-import { red, yellow } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
   root: {},
   media: {
-    height: 0,
-    paddingTop: '56.25%',
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  },
-  avatar: {
-    backgroundColor: yellow[500],
+    width: 150,
+    padding: 10,
   },
 }));
 
 export default function RecipeReviewCard() {
   const classes = useStyles();
+  const preventDefault = (event) => event.preventDefault();
 
   return (
     <div>
       <Card className={classes.root}>
-        <CardHeader
-          avatar={
-          
-            <Avatar alt="Intertek Alchemy" variant="square" src="/static/images/alchemy.png"  className={classes.avatar} />
-          }
-          title="Intertek Alchemy"
-          subheader="September 14, 2016"
-        />
-        <CardMedia
-          className={classes.media}
-          image="/static/images/paella.jpg"
-          title="Paella dish"
-        />
         <CardContent>
+          <Link href="#" onClick={preventDefault}>
+            <CardMedia
+              component="img"
+              className={classes.media}
+              image="/static/images/alchemy.png"
+              title="Intertek Alchemy"
+            />
+          </Link>
           <Typography variant="body2" color="textSecondary" component="p">
             This impressive paella is a perfect party dish and a fun meal to cook together with your
             guests. Add 1 cup of frozen peas along with the mussels, if you like.
