2b866bed260aafb9a52048d667fff9c82fbe40b9
Thon Becker <thon.becker@gmail.com>
2021-01-07 12:42:34 +0000

    Update projen

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,6 @@ const { AwsCdkTypeScriptApp, CdkApprovalLevel } = require('projen');
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.83.0',
   cdkVersionPinning: true,
-  jest: false,
   deps: ['cdk-simplewebsite-deploy'],
   requireApproval: CdkApprovalLevel.NEVER,
   name: 'BeckerGamesWeb',
