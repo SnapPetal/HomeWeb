0f1bba24d1d5e36659fafb1f6e6701de3519a115
Thon Becker <thon.becker@gmail.com>
2021-01-07 02:48:01 +0000

    Update code

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { App, Construct, Stack, StackProps } from "@aws-cdk/core";
-import { CreateBasicSite } from "cdk-simplewebsite-deploy";
+import { App, Construct, Stack, StackProps } from '@aws-cdk/core';
+import { CreateBasicSite } from 'cdk-simplewebsite-deploy';
 
 export class BeckerGamesPageStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
-    new CreateBasicSite(this, "beckergame-website", {
-      websiteFolder: "./src/website",
-      indexDoc: "index.html",
-      hostedZoneDomain: "beckergames.net",
-      websiteDomain: "beckergames.net",
-      websiteSubDomain: "www.beckergames.net",
+    new CreateBasicSite(this, 'beckergame-website', {
+      websiteFolder: './src/website',
+      indexDoc: 'index.html',
+      hostedZoneDomain: 'beckergames.net',
+      websiteDomain: 'beckergames.net',
+      websiteSubDomain: 'www.beckergames.net',
       encryptBucket: true,
     });
   }
@@ -22,6 +22,6 @@ const prodEnv = {
 
 const app = new App();
 
-new BeckerGamesPageStack(app, "beckergames-page-stack", { env: prodEnv });
+new BeckerGamesPageStack(app, 'beckergames-page-stack', { env: prodEnv });
 
 app.synth();
