34c0f9ca8919bac892a86e5e0fbb2f2a771ab14a
Thon Becker <thon.becker@gmail.com>
2021-01-18 02:58:09 +0000

    Add certifications in about

--- client/src/AboutPage.jsx
+++ client/src/AboutPage.jsx
@@ -1,13 +1,38 @@
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
 export default function AboutPage() {
   return (
     <div>
-      <Typography paragraph>
-          As a web developer, I enjoy using my obsessive attention to detail,
-          my unequivocal love for making things, and my mission-driven work ethic to literally change the world.
-          That's why I’m excited to make a big impact at a high growth company.
+      <Typography component={'span'}>
+          More than 20 years as Senior Developer and more than 10 years as Software Architect based on Microsoft Technology.
+          Cloud Solution architect and Senior developer in AWS projects.
+          Extensive expertise in Product Development, Engineering & Full Development Lifecycle.
+        <ul>
+          <li>Expert in .NET, C#, Microsoft Azure, Windows 8.x/10, Windows Phone 8.x/10</li>
+          <li>Technical Team Leader</li>
+          <li>Technical Design Architect & Developer</li>
+          <li>Scrum</li>
+          <li>Microsoft TFS 2013/2015/VS online/ALM</li>
+          <li>Code Analysis/Unit Tests</li>
+          <li>Design and implementation of tablet and mobile application's</li>
+          <li>Design and Implementation of Web Api's</li>
+          <li>Strategic Technology Planning</li>
+          <li>Ability to lead development team and deliver quality products on time.</li>
+          <li>Ability to specify, design, plan and implement software solutions (client/server side)</li>
+          <li>Design and implementation of applications and system architectures</li>
+          <li>Design and implementation of Cloud solutions (AWS).</li>
+          <li>Design and implementation of application with latest user interfaces</li>
+          <li>R&D - Web andClient/Server with latest and emerging technologies.</li>
+        </ul>
       </Typography>
+      <Typography variant="h6" gutterBottom>
+        Certifications
+      </Typography>
+      <Paper>
+        <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="15c62f1f-4211-483c-9e7e-eae310f78cfd" data-share-badge-host="https://www.youracclaim.com" />
+        <div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="696d72cf-0401-4b0c-83e0-50ac161ae90c" data-share-badge-host="https://www.youracclaim.com" />
+      </Paper>
     </div>
   );
 }
