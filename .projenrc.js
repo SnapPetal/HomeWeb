7c6a038db9e78b9446d9ea12f883622c15e5b8aa
Thon Becker <thon.becker@gmail.com>
2021-06-09 22:24:41 -0500

    chore(deps):upgrade packages

--- .projenrc.js
+++ .projenrc.js
@@ -2,7 +2,6 @@ const { AwsCdkTypeScriptApp, CdkApprovalLevel } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.103.0',
-  cdkVersionPinning: true,
   deps: ['cdk-simplewebsite-deploy'],
   requireApproval: CdkApprovalLevel.NEVER,
   name: 'HomeWeb',
