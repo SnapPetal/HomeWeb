810dad152254672e002aa35f81ba9f29d357ebb8
Thon Becker <thon.becker@gmail.com>
2021-06-30 23:50:52 -0500

    chore: project created with projen

--- infrastructure/bin/nextjs-cdk.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { NextjsCdkStack } from '../lib/nextjs-cdk-stack';
-
-const app = new cdk.App();
-new NextjsCdkStack(app, 'home-page-stack');
