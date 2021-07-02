f2d47b3d5d2d7bb52237c6ae93a83bead84d31dd
Thon Becker <thon.becker@gmail.com>
2021-07-01 22:35:47 -0500

    feat: add rangom bible verse

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,23 @@
 import Head from 'next/head'
 import Link from "next/link"
 
-
-const Pokemons = ({ data }) => {
-
+const BibleVerse = ({ data }) => {
   return (
     <section>
       <Head>
-        <title>PokéServerless</title>
+        <title>Thon Becker</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <h2>Pokémons</h2>
-      <ul style={{ listStyle: "none" }}>
-        {data.results.map((pokemon, _index) => (
-          <li key={pokemon.name} style={{ textTransform: "capitalize" }}>{_index + 1} <Link href={pokemon.name}><a style={{ fontWeight: "bold", fontFamily: '"Comic Sans MS", cursive, sans-serif', color: "tomato", textDecoration: "none" }}>{pokemon.name}</a></Link></li>
-        ))}
-      </ul>
-      <h3>Total of {data.count} Pokémons...</h3>
+      <body>
+        <h2>Verse of the Day</h2>
+        <p>{data.results}</p>
+      </body>
     </section>
   )
 }
 
 export async function getStaticProps() {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1118')
+  const res = await fetch('https://www.ourmanna.com/verses/api/get?format=json&order=random');
   const data = await res.json()
 
   return {
@@ -32,4 +27,4 @@ export async function getStaticProps() {
   }
 }
 
-export default Pokemons
+export default BibleVerse
