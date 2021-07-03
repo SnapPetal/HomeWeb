a08045214a2e1e1e28598940b3cb409716317e7a
Thon Becker <thon.becker@gmail.com>
2021-07-02 23:34:46 -0500

    fix: adjust axios fetch

--- components/BibleVerse.js
+++ components/BibleVerse.js
@@ -4,21 +4,16 @@ import { useQuery } from "react-query";
 import Typography from '@material-ui/core/Typography';
 
 const fetchBibleVerse = async () => {
-    return await axios.get('https://www.ourmanna.com/verses/api/get?format=json&order=random', {
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
-    });
+    return await axios.get('https://quotes.rest/bible/vod.json');
 };
 
 export default function BibleVerse() {
-    const { data, status } = useQuery("verse", fetchBibleVerse);
+    const { isLoading, isError, data, error } = useQuery("verse", fetchBibleVerse);
     if (!data) return null;
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             <h2>Verse of the Day</h2>
-            <p>{data.verse.details.text}</p>
-            <p>{data.verse.details.reference} {data.verse.details.version}</p>
+            <p>{data.data.contents.verse}</p>
         </Typography>
     );
 }
