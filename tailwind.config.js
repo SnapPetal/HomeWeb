b857a3441c33854288bed1573346baf809f5e78f
EC2 Default User <ec2-user@ip-172-31-29-46.ec2.internal>
2022-05-21 20:43:09 +0000

    feat(web):add new template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   content: [
-    './public/index.html',
-    './src/**/*.{html,js}',
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
+  theme: {
+    extend: {
+      inset: {
+        '100': '100%',
+      },
+
+      padding: {
+        '120': '120px',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
 }
