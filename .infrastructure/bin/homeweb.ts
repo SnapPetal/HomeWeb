bae4ac2c22ab60295e02514f7f8800422fce4cf2
EC2 Default User <ec2-user@ip-172-31-29-46.ec2.internal>
2022-06-03 00:39:04 +0000

    chore: upgrade packages

--- .infrastructure/bin/homeweb.ts
+++ .infrastructure/bin/homeweb.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { HomeWebStack } from '../lib/homeweb-stack';
 
 const app = new cdk.App();
-new HomeWebStack(app, 'HomeWebStack',{ 
+new HomeWebStack(app, 'thonbecker-page-stack',{ 
   env: { 
     account: process.env.CDK_DEFAULT_ACCOUNT, 
     region: process.env.CDK_DEFAULT_REGION 
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -58,50 +58,6 @@ $(function() {
         });
     });
     
-    
-    //====== Magnific Popup
-    
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-        // other options
-    });
-    
-    
-    //===== Slick
-
-    $('.testimonial-active').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        speed: 800,
-        arrows: false,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    arrows: false,
-                }
-            }
-        ]
-    });
-    
-    
     //===== Back to top
     
     // Show or hide the sticky footer button
@@ -112,17 +68,6 @@ $(function() {
             $('.back-to-top').fadeOut(200)
         }
     });
-    
-    
-    //Animate the scroll to yop
-    $('.back-to-top').on('click', function(event) {
-        event.preventDefault();
-        
-        $('html, body').animate({
-            scrollTop: 0,
-        }, 1500);
-    });
-    
     
     //===== 
     
