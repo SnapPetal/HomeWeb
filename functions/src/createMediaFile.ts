4b65e62a0bd8624b772c26d7a755785cc7b7f5f3
Thon Becker <me@thonbecker.com>
2023-11-11 09:20:01 +0000

    chore(functions): code cleanup

--- functions/src/createMediaFile.ts
+++ functions/src/createMediaFile.ts
@@ -42,7 +42,7 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<SendVoiceRes
     VoiceId: voice
   }
 
-  return await polly.synthesizeSpeech(params).promise().then(audio => {
+  await polly.synthesizeSpeech(params).promise().then(audio => {
     if (audio.AudioStream instanceof Buffer) {
       return {
         statusCode: 200,
