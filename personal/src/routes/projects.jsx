3555912404fca84bb1edd2dbeb83a10145b90132
Thon Becker <me@thonbecker.com>
2023-11-11 01:20:06 +0000

    fix(frontend): adjust call

--- personal/src/routes/projects.jsx
+++ personal/src/routes/projects.jsx
@@ -10,6 +10,7 @@ export async function loader() {
     header: {
       Accept: "application/json",
       "Content-Type": "text/plain",
+      "User-Agent": "thonbecker.com",
     },
   });
   const { joke } = await dadJokeResponse.json();
