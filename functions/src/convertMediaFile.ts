81536f306fef76bd5beb27ed385e8cbaa7c69b2c
Thon Becker <me@thonbecker.com>
2023-12-09 23:53:07 +0000

    chore: adjust type

--- functions/src/convertMediaFile.ts
+++ functions/src/convertMediaFile.ts
@@ -1,54 +1,47 @@
-import * as AWS from 'aws-sdk';
-import * as sharp from 'sharp';
+import * as AWS from 'aws-sdk'
+import * as sharp from 'sharp'
 
-type S3Detail = {
-  eventVersion: string;
-  eventSource: string;
-  awsRegion: string;
-  eventTime: string;
-  eventName: string;
-  userIdentity: {
-    principalId: string;
-  };
-  requestParameters: {
-    sourceIPAddress: string;
-  };
-  responseElements: {
-    'x-amz-request-id': string;
-    'x-amz-id-2': string;
-  };
-  s3: {
-    s3SchemaVersion: string;
-    configurationId: string;
-    bucket: {
-      name: string;
-      ownerIdentity: {
-        principalId: string;
-      };
-      arn: string;
-    };
-    object: {
-      key: string;
-      size: number;
-      eTag: string;
-      versionId: string;
-      sequencer: string;
-    };
-  };
-};
+interface S3EventDetail {
+  version: string
+  bucket: {
+    name: string
+  }
+  object: {
+    key: string
+    size: number
+    etag: string
+    sequencer: string
+  }
+  'request-id': string
+  requester: string
+  'source-ip-address': string
+  reason: string
+}
 
-export const handler = async (event: S3Detail): Promise<Buffer> => {
-  console.log(event);
-  const bucket = event.s3.bucket.name;
-  const key = decodeURIComponent(event.s3.object.key.replace(/\+/g, ' '));
+interface CloudWatchS3Event {
+  version: string
+  id: string
+  'detail-type': string
+  source: string
+  account: string
+  time: string
+  region: string
+  resources: string[]
+  detail: S3EventDetail
+}
+
+export const handler = async (event: CloudWatchS3Event): Promise<Buffer> => {
+  console.log(event)
+  const bucket = event.detail.bucket.name
+  const key = decodeURIComponent(event.detail.object.key.replace(/\+/g, ' '))
 
   const params: AWS.S3.GetObjectRequest = {
     Bucket: bucket,
     Key: key
-  };
+  }
 
-  const S3 = new AWS.S3();
-  const { Body } = await S3.getObject(params).promise();
-  const inputData = Body as Buffer;
-  return await sharp(inputData).jpeg().toBuffer();
-};
+  const S3 = new AWS.S3()
+  const { Body } = await S3.getObject(params).promise()
+  const inputData = Body as Buffer
+  return await sharp(inputData).jpeg().toBuffer()
+}
