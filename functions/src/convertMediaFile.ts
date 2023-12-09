701535b779cf13ae4d832a9198771ca21349121d
Thon Becker <me@thonbecker.com>
2023-12-09 23:43:56 +0000

    chore: modify convert type

--- functions/src/convertMediaFile.ts
+++ functions/src/convertMediaFile.ts
@@ -1,19 +1,54 @@
-import * as AWS from 'aws-sdk'
-import * as sharp from 'sharp'
-import { type S3Event } from 'aws-lambda'
+import * as AWS from 'aws-sdk';
+import * as sharp from 'sharp';
 
-export const handler = async (event: S3Event): Promise<Buffer> => {
+type S3Detail = {
+  eventVersion: string;
+  eventSource: string;
+  awsRegion: string;
+  eventTime: string;
+  eventName: string;
+  userIdentity: {
+    principalId: string;
+  };
+  requestParameters: {
+    sourceIPAddress: string;
+  };
+  responseElements: {
+    'x-amz-request-id': string;
+    'x-amz-id-2': string;
+  };
+  s3: {
+    s3SchemaVersion: string;
+    configurationId: string;
+    bucket: {
+      name: string;
+      ownerIdentity: {
+        principalId: string;
+      };
+      arn: string;
+    };
+    object: {
+      key: string;
+      size: number;
+      eTag: string;
+      versionId: string;
+      sequencer: string;
+    };
+  };
+};
+
+export const handler = async (event: S3Detail): Promise<Buffer> => {
   console.log(event);
-  const bucket = event.Records[0].s3.bucket.name
-  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '))
+  const bucket = event.s3.bucket.name;
+  const key = decodeURIComponent(event.s3.object.key.replace(/\+/g, ' '));
 
   const params: AWS.S3.GetObjectRequest = {
     Bucket: bucket,
     Key: key
-  }
+  };
 
-  const S3 = new AWS.S3()
-  const { Body } = await S3.getObject(params).promise()
-  const inputData = Body as Buffer
-  return await sharp(inputData).jpeg().toBuffer()
-}
+  const S3 = new AWS.S3();
+  const { Body } = await S3.getObject(params).promise();
+  const inputData = Body as Buffer;
+  return await sharp(inputData).jpeg().toBuffer();
+};
