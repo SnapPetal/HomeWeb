25eb1e153ce2f65e7870599b5c14fe8bf83fe5cf
Thon Becker <me@thonbecker.com>
2023-12-09 16:02:22 +0000

    chore: add logging for event

--- functions/src/convertMediaFile.ts
+++ functions/src/convertMediaFile.ts
@@ -3,6 +3,7 @@ import * as sharp from 'sharp'
 import { type S3Event } from 'aws-lambda'
 
 export const handler = async (event: S3Event): Promise<Buffer> => {
+  console.log(event);
   const bucket = event.Records[0].s3.bucket.name
   const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '))
 
