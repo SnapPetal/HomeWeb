f2b3a7d15bf9d5913b383e03dff834ceb636728e
Thon Becker <me@thonbecker.com>
2024-01-13 18:55:30 +0000

    chore(functions): linter fixes

--- functions/src/processMediaFile.ts
+++ functions/src/processMediaFile.ts
@@ -10,11 +10,11 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<string> => {
   const objects = await s3.listObjectsV2({ Bucket: bucketName }).promise()
 
   // Extract the names of the files
-  const fileNames = objects.Contents?.map(file => file.Key) || []
+  const fileNames = objects.Contents?.map(file => file.Key) ?? []
 
   // Filter out only .jpg and .heic files
   const filteredFileNames = fileNames.filter(fileName =>
-    fileName && (fileName.endsWith('.jpg') || fileName.endsWith('.heic'))
+    fileName !== null && fileName !== undefined && (fileName.endsWith('.jpg') || fileName.endsWith('.heic'))
   )
 
   // Return the names in a JSON format
