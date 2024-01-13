563949a9625e42b8f70c5c8e39935bcfef7c9043
Thon Becker <me@thonbecker.com>
2024-01-13 18:50:58 +0000

    feat(functions): change to read files

--- functions/src/processMediaFile.ts
+++ functions/src/processMediaFile.ts
@@ -1,38 +1,22 @@
 import { S3 } from 'aws-sdk'
 import { type APIGatewayProxyEvent } from 'aws-lambda'
-import { Readable } from 'stream'
-import csv from 'csvtojson'
 
 const s3 = new S3()
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<string> => {
   console.log(event)
-  const bucketName = 'cdn-page-stack-inventorybucketa869b8cb-4bxdk5l41ale'
+  const bucketName = 'cdn-page-stack-mediabucketbcbb02ba-cxens3y5o2lj'
 
   const objects = await s3.listObjectsV2({ Bucket: bucketName }).promise()
 
-  if (objects.Contents !== undefined && objects.Contents !== null && objects.Contents.length > 0) {
-    const csvFiles = objects.Contents.filter(file => file.Key !== undefined && file.Key !== null && file.Key.endsWith('.csv'))
+  // Extract the names of the files
+  const fileNames = objects.Contents?.map(file => file.Key) || []
 
-    const latestFile = csvFiles[0]
+  // Filter out only .jpg and .heic files
+  const filteredFileNames = fileNames.filter(fileName =>
+    fileName && (fileName.endsWith('.jpg') || fileName.endsWith('.heic'))
+  )
 
-    if (latestFile.Key !== undefined && latestFile.Key !== null && latestFile.Key.trim() !== '') {
-      const file = await s3.getObject({ Bucket: bucketName, Key: latestFile.Key }).promise()
-
-      if (file.Body !== undefined && file.Body !== null) {
-        const stream = new Readable()
-        stream.push(file.Body)
-
-        const json = await csv().fromStream(stream)
-
-        return JSON.stringify(json)
-      } else {
-        throw new Error('No file body found')
-      }
-    } else {
-      throw new Error('No csv files found')
-    }
-  } else {
-    throw new Error('No objects found')
-  }
+  // Return the names in a JSON format
+  return JSON.stringify(filteredFileNames)
 }
