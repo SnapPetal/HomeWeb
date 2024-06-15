e8eb42b85744ec5eda660b8408df46e65e8075d5
Thon Becker <me@thonbecker.com>
2024-06-15 01:28:23 +0000

    chore(infrastructure): adjust cognito

--- .infrastructure/lib/idp-stack.ts
+++ .infrastructure/lib/idp-stack.ts
@@ -11,39 +11,14 @@ export class IdpStack extends Stack {
 
     const personalUserPool = new cognito.UserPool(this, "personalUserpool", {
       userPoolName: "personalUserpool",
-      signInCaseSensitive: false,
-      signInAliases: { username: true,
-        email: true,
-        phone: false,
-       },
-      autoVerify: { email: true },
+      selfSignUpEnabled: true,
+      autoVerify: { email: false },
+      signInAliases: { email: true },
       standardAttributes: {
         fullname: {
           required: true,
           mutable: true,
         },
-        address: {
-          required: true,
-          mutable: true,
-        },
-      },
-      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
-      selfSignUpEnabled: true,
-      userVerification: {
-        emailSubject:
-          "Welcome to Thon Becker Solutions - Please Verify Your Email",
-        emailBody:
-          "Hello, and welcome to Thon Becker Solutions! To get started, please verify your email address. Your verification code is {####}.",
-        emailStyle: cognito.VerificationEmailStyle.CODE,
-        smsMessage:
-          "Welcome to Thon Becker Solutions! Your verification code is {####}.",
-      },
-      userInvitation: {
-        emailSubject: "You're Invited to Join Thon Becker Solutions",
-        emailBody:
-          "Hello {username}, you have been invited to join Thon Becker Solutions. To get started, please use the following temporary password: {####}.",
-        smsMessage:
-          "Hello {username}, welcome to Thon Becker Solutions. Your temporary password is {####}.",
       },
     });
 
